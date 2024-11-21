bf42d7466b2a5080685370a3c06d6961885a3112
novaritzz <155249127+novaritzz@users.noreply.github.com>
2024-11-21 13:01:27 +0100

    Add app service plan and webapp

---
+++ lab06-iac/app_service.tf
@@ -0,0 +1,26 @@
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = "${var.lastname}-asp-${var.environment}-${lower(replace(var.location, " ", ""))}-01"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = var.app_service_os
+
+  sku {
+    tier = "Free"
+    size = var.app_service_sku
+  }
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "${var.lastname}-webapp-${var.environment}-${lower(replace(var.location, " ", ""))}-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_app_service_plan.app_plan.id
+
+  site_config {
+    ftps_state        = "Disabled"
+    min_tls_version   = var.tls_version
+    linux_fx_version  = var.runtime_version
+  }
+
+  https_only = var.https_only
+}
