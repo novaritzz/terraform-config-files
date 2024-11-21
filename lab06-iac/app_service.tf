38771a3cb7af460479fc4dd6828bd02fa92cb313
novaritzz <155249127+novaritzz@users.noreply.github.com>
2024-11-21 13:06:43 +0100

    Change deprecated resource

--- lab06-iac/app_service.tf
+++ lab06-iac/app_service.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_plan" "app_plan" {
+resource "azurerm_service_plan" "app_plan" {
   name                = "${var.lastname}-asp-${var.environment}-${lower(replace(var.location, " ", ""))}-01"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
