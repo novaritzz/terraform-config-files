9e72ca361a553501e3853fc8d25b5e73f9fe2557
novaritzz <155249127+novaritzz@users.noreply.github.com>
2024-11-21 13:12:54 +0100

    Update app_service.tf

--- lab06-iac/app_service.tf
+++ lab06-iac/app_service.tf
@@ -2,19 +2,15 @@ resource "azurerm_service_plan" "app_plan" {
   name                = "${var.lastname}-asp-${var.environment}-${lower(replace(var.location, " ", ""))}-01"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = var.app_service_os
-
-  sku {
-    tier = "Free"
-    size = var.app_service_sku
-  }
+  os_type             = var.app_service_os
+  sku_name            = var.app_service_sku
 }
 
 resource "azurerm_linux_web_app" "webapp" {
   name                = "${var.lastname}-webapp-${var.environment}-${lower(replace(var.location, " ", ""))}-01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_app_service_plan.app_plan.id
+  service_plan_id     = azurerm_service_plan.app_plan.id
 
   site_config {
     ftps_state        = "Disabled"
